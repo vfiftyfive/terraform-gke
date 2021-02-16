ae8214575068b2b1549746df727f4b157d5d3a95
Nicolas Vermande <nicolasvermande@Nicolas-Vermandes-MacBook-Pro.local>
2021-02-15 23:51:06 -0800

    Add kubernetes provider

---
+++ kubernetes.tf
@@ -0,0 +1,9 @@
+locals {
+  cluster_ca_certificate = base64decode(google_container_cluster.vpc_native_cluster.master_auth[0].cluster_ca_certificate)
+}
+
+resource "kubernetes_secret" "example" {
+  metadata {
+    name = "terraform-example"
+  }
+}
